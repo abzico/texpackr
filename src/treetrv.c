05d631c9008003171977dc2c478629116667a84d
Wasin Thonkaew <haxpor@gmail.com>
2019-03-12 19:42:17 +0800

    Fix tree traversal to not mess around with freeing image data

--- src/treetrv.c
+++ src/treetrv.c
@@ -1,5 +1,7 @@
 #include <stdlib.h>
+#include <png.h>
 #include "texpackr_internal.h"
+#include "png_util.h"
 
 void texpackr_free_all_nodes(texpackr_node* n)
 {
@@ -8,9 +10,12 @@ void texpackr_free_all_nodes(texpackr_node* n)
 		texpackr_free_all_nodes(n->child[0]);
 		texpackr_free_all_nodes(n->child[1]);
 
-		// delete the node
+		// delete child nodes
 		n->child[0] = NULL;
 		n->child[1] = NULL;
+
+		printf("n rect: %d, %d, %d, %d\n", n->rc.left, n->rc.top, n->rc.right, n->rc.bottom);
+
 		free(n);
 		n = NULL;
 	}
