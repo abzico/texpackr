aafd81abd378f3a7bd6363b845f42ef50cab1613
Wasin Thonkaew <haxpor@gmail.com>
2019-03-14 05:33:27 +0800

    Update test program to include reading GRAY png image, better organize

--- test/texpackr_test.c
+++ test/texpackr_test.c
@@ -1,3 +1,5 @@
+// test program for capability of texpackr
+// you should not see any "Error inserting ..." on console
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -27,45 +29,44 @@ int main (int argc, char** argv)
 	bool result = false;
 	texpackr_sheet* sheet = texpackr_sheet_new(SHEET_MAX_WIDTH, SHEET_MAX_HEIGHT);
 
-	// test individual insert
+	// 1. test individual insert
+	printf("Test individual insertion\n");
 	result = texpackr_sheet_insert_img(sheet, "images/1.png");
 	if (!result)
-		fprintf(stderr, "Error inserting image 1.png\n");
+		fprintf(stderr, "Error inserting image1.png\n");
 
 	result = texpackr_sheet_insert_img(sheet, "images/2.png");
 	if (!result)
-		fprintf(stderr, "Error inserting image 2.png\n");
+		fprintf(stderr, "Error inserting image2.png\n");
 
 	result = texpackr_sheet_insert_img(sheet, "images/3.png");
 	if (!result)
-		fprintf(stderr, "Error inserting image 3.png\n");
+		fprintf(stderr, "Error inserting image3.png\n");
 	
 	result = texpackr_sheet_insert_img(sheet, "images/4.png");
 	if (!result)
-		fprintf(stderr, "Error inserting image 4.png\n");
+		fprintf(stderr, "Error inserting image4.png\n");
 
 	result = texpackr_sheet_insert_img(sheet, "images/5.png");
 	if (!result)
-		fprintf(stderr, "Error inserting image 5.png\n");
+		fprintf(stderr, "Error inserting image5.png\n");
 
 	result = texpackr_sheet_insert_img(sheet, "images/6.png");
 	if (!result)
-		fprintf(stderr, "Error inserting image 6.png\n");
+		fprintf(stderr, "Error inserting image6.png\n");
 
 	result = texpackr_sheet_insert_img(sheet, "images/7.png");
 	if (!result)
-		fprintf(stderr, "Error inserting image 7.png\n");
+		fprintf(stderr, "Error inserting image7.png\n");
 
-	result = texpackr_sheet_insert_img(sheet, "images/9.png");
+	result = texpackr_sheet_insert_img(sheet, "images/8.png");
 	if (!result)
-		fprintf(stderr, "Error inserting image 9.png\n");
+		fprintf(stderr, "Error inserting image8.png\n");
 
 	print_all_sprites(sheet);
 
 	// clear then we gonna test batch insert
-	printf("clear sheet\n");
 	texpackr_sheet_clear(sheet);
-	print_all_sprites(sheet);
 
 	// form the array of string for using in batch insertion
 #define BATCH_IMG_SIZE 8
@@ -79,8 +80,21 @@ int main (int argc, char** argv)
 		"images/7.png",
 		"images/8.png"
 	};
-	// batch insert
-	texpackr_sheet_batch_imgs(sheet, (const char**)&batch_img_filenames, BATCH_IMG_SIZE);
+	// 2. batch insert
+	printf("Test batch insertion\n");
+	result = texpackr_sheet_batch_imgs(sheet, (const char**)&batch_img_filenames, BATCH_IMG_SIZE);
+	if (!result)
+		fprintf(stderr, "Error batch inserting images\n");
+	print_all_sprites(sheet);
+
+	// clear then we gonna test batch insert
+	texpackr_sheet_clear(sheet);
+	
+	// 3. test inserting GRAY (1 channel) png image
+	printf("Test inserting GRAY format (1 channel) png image\n");
+	result = texpackr_sheet_insert_img(sheet, "images/gray.png");
+	if (!result)
+		fprintf(stderr, "Error inserting gray.png\n");
 	print_all_sprites(sheet);
 
 	// done, then clear sheet
