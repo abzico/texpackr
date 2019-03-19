876c94d3d22e5cb865d4402f481e508723393fe8
Wasin Thonkaew <haxpor@gmail.com>
2019-03-19 19:13:47 +0800

    Fix missing included file 'sprite.h'

---
+++ include/texpackr/sprite.h
@@ -0,0 +1,20 @@
+#ifndef TEXPACKR_SPRITE_H_
+#define TEXPACKR_SPRITE_H_
+
+#include "texpackr/types.h"
+
+/*
+ * Free individual sprite structure.
+ *
+ * \param sp sprite
+ */
+extern void texpackr_sprite_free(texpackr_sprite* sp);
+
+/*
+ * Free individual sprite's internals.
+ *
+ * \param sp sprite.
+ */
+extern void texpackr_sprite_free_internals(texpackr_sprite* sp);
+
+#endif
